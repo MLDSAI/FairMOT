3789c20684dfa580686d556fe2a6c5223731de87
Richard Abrich <richard.abrich@mldsai.com>
2020-10-03 17:00:58 -0400

    / -> //

--- src/lib/models/decode.py
+++ src/lib/models/decode.py
@@ -36,7 +36,7 @@ def _topk(scores, K=40):
     topk_xs   = (topk_inds % width).int().float()
       
     topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
-    topk_clses = (topk_ind / K).int()
+    topk_clses = (topk_ind // K).int()
     topk_inds = _gather_feat(
         topk_inds.view(batch, -1, 1), topk_ind).view(batch, K)
     topk_ys = _gather_feat(topk_ys.view(batch, -1, 1), topk_ind).view(batch, K)
